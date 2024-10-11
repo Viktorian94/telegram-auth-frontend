67270414991528f794a1d3555af66a8a63aaee09
Viktor <ogivita11q@gmail.com>
2024-10-11 22:47:51 +0300

    fix undefind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,9 @@
 import Script from "next/script";
 
 const Home = () => {
-
+  const url = process.env.NEXT_PUBLIC_API_URL as string
+  if (!url) return;
+  
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-black">
       <h1 className="text-4xl font-bold mb-8">Авторизація через Telegram</h1>
@@ -14,7 +16,7 @@ const Home = () => {
         data-size="large"
         data-userpic="true"
         data-request-access="write"
-        data-auth-url={`${process.env.NEXT_PUBLIC_API_URL}/auth/telegram/callback`}
+        data-auth-url={`${url}/auth/telegram/callback`}
         strategy="afterInteractive"
       />
     </div>
